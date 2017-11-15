3d3482c75a506422a963600924b67cb4569c6248
antschreier82 <as0858525@otc.edu>
2017-11-14 22:51:05 -0600

    updates

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var path = require('path');
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
+    res.sendFile(path.join(__dirname + "/author.html"));
+    res.sendFile(path.join(__dirName + "/styles.css"));
 });
 
 app.listen(3000);
