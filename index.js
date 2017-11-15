a779b3fbf13d69c4dbc721dedc4cd3362251786f
antschreier82 <as0858525@otc.edu>
2017-11-14 23:37:55 -0600

    Completed

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-//app.use('/', express.static(__dirname));
+app.use('/', express.static(__dirname));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + "/index.html"));
-    
-    app.get ('/', function (req, res) {
-        res.sendFile(path.join(__dirname + "/author.html"));
-    });
 });
 
-app.get ('/', function (req, res) {
-    res.sendFile(path.join(__dirname + "/author.html"));
-});
-
-app.get ('/', function (req, res) {
-    res.sendFile(path.join(__dirname + "/user.html"));
-})
 app.listen(3000);
